1201720d2aa39ec77f786ccabec181f47bed10d0
VladLightning <vladlen4102006@gmail.com>
2024-06-24 19:33:55 +0300

    ManageCoins now checks available upgrades

--- Assets/Scripts/ManageCoins.cs
+++ Assets/Scripts/ManageCoins.cs
@@ -1,9 +1,11 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ManageCoins : MonoBehaviour
 {
     [SerializeField] private TMP_Text _coinsDisplay;
+    [SerializeField] private Button[] _upgradeButtons;
     private Coins _coins;
 
     private void Start()
@@ -15,12 +17,31 @@ public class ManageCoins : MonoBehaviour
     {
         _coins.AddCoins();
         _coinsDisplay.text = _coins.Amount.ToString();
+        CheckAvailableUpgrades();
     }
 
     public void DecreaseAmount(int amount)
     {
         _coins.Purchase(amount);
         _coinsDisplay.text = _coins.Amount.ToString();
+        CheckAvailableUpgrades();
+    }
+
+    private void CheckAvailableUpgrades()
+    {
+        for(int i = 0; i < _upgradeButtons.Length; i++)
+        {
+            var upgradeInfo = _upgradeButtons[i].GetComponent<UpgradeInfo>();
+
+            if (upgradeInfo.Price <= _coins.Amount)
+            {
+                _upgradeButtons[i].interactable = true;
+            }
+            else if(upgradeInfo.Price > _coins.Amount && _upgradeButtons[i].interactable == true)
+            {
+                _upgradeButtons[i].interactable = false;
+            }
+        }
     }
 
     public Coins Coins
